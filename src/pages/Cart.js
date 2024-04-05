e27acb119df423cdafe4c63f417a12573cd30d8b
janagagan <janagagan@gmail.com>
2024-04-05 13:40:50 +0800

    Updated cart codes

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,25 +1,22 @@
 import React, { useState, useEffect } from 'react';
 import { Container, Card, Button, Row, Col } from 'react-bootstrap';
-import { AiOutlinePlus, AiOutlineMinus, AiOutlineDelete } from 'react-icons/ai';
 import Swal from 'sweetalert2'; 
-import { Link, navigate, useNavigate } from "react-router-dom"; 
+import { Link, useNavigate } from "react-router-dom"; 
 
 function Cart() {
-    const [cart, setCart] = useState([]);
+    const [cart, setCart] = useState({});
     const [totalPrice, setTotalPrice] = useState(0);
-    const [orderCreated, setOrderCreated] = useState(false); 
+    const [isLoading, setIsLoading ] = useState(true);
     const navigate = useNavigate();
 
     useEffect(() => {
         fetchCartData();
     }, []);
 
-    useEffect(() => {
-        const totalPrice = cart.reduce((total, item) => total + item.subtotal, 0);
-        setTotalPrice(totalPrice);
-    }, [cart]);
+
 
     const fetchCartData = () => {
+        setIsLoading(true)
         fetch(`${process.env.REACT_APP_API_URL}/carts/get-cart`, {
             method: "GET",
             headers: {
@@ -27,47 +24,32 @@ function Cart() {
                 Authorization: `Bearer ${localStorage.getItem('token')}`
             }
         })
-        .then(res => {
-            if (!res.ok) {
-                throw new Error('Failed to fetch cart data');
-            }
-            return res.json();
-        })
-        .then(data => {
-            if (data.cart && data.cart.cartItems && Array.isArray(data.cart.cartItems)) {
-                const updatedCart = data.cart.cartItems.map(item => ({
-                    productId: item.productId ? item.productId._id : null,
-                    name: item.productId ? item.productId.name : 'Product Name Unavailable',
-                    price: item.productId ? item.productId.price : 0,
-                    quantity: item.quantity,
-                    subtotal: item.subtotal
-                }));
-                setCart(updatedCart);
-            } else {
-                throw new Error('Invalid cart data format');
-            }
-        })
-        .catch(err => {
-            console.error('Error fetching cart data:', err);
-            Swal.fire({
-                icon: 'error',
-                title: 'Error',
-                text: 'Failed to fetch cart data.'
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                setCart(data.cart);
+                const totalPrice = data.cart.cartItems.reduce((total, item) => total + item.subtotal, 0);
+                setTotalPrice(totalPrice);
+                setIsLoading(false);
+               
+            })
+            .catch(err => {
+                setIsLoading(false)
+                Swal.fire({
+                    icon: 'error',
+                    title: 'Error',
+                    text: 'Failed to fetch cart data.'
+                });
             });
-        });
     };
-    
-    
-    
-    
+     console.log("data from cart state", cart)
     const handleUpdateQuantity = (productId, newQuantity) => {
         newQuantity = parseInt(newQuantity) || 0;
-    
         if (newQuantity < 0) {
             newQuantity = 0;
         }
-    
-        fetch(`${process.env.REACT_APP_API_URL}/carts/update-cart-quantity`, {
+
+        fetch(`http://ec2-18-217-154-136.us-east-2.compute.amazonaws.com/b5/cart/update-cart-quantity`, {
             method: "PATCH",
             headers: {
                 "Content-Type": "application/json",
@@ -84,10 +66,7 @@ function Cart() {
                 setCart(prevCart => {
                     const updatedCart = prevCart.map(item => {
                         if (item.productId === productId) {
-                            console.log('item.price:', item.price);
-                            console.log('newQuantity:', newQuantity);
                             const updatedSubtotal = item.price * newQuantity;
-                            console.log('updatedSubtotal:', updatedSubtotal);
                             return { ...item, quantity: newQuantity, subtotal: updatedSubtotal };
                         }
                         return item;
@@ -110,11 +89,10 @@ function Cart() {
             });
         });
     };
-    
-    
+
     const handleRemoveItem = (productId) => {
-        fetch(`${process.env.REACT_APP_API_URL}/carts/${productId}/remove-from-cart`, {
-            method: "PATCH",
+        fetch(`http://ec2-18-217-154-136.us-east-2.compute.amazonaws.com/b5/cart/:productId/remove-from-cart`, {
+            method: "DELETE",
             headers: {
                 "Content-Type": "application/json",
                 Authorization: `Bearer ${localStorage.getItem('token')}`
@@ -123,7 +101,7 @@ function Cart() {
         .then(res => res.json())
         .then(data => {
             if (data.message === 'Item removed from cart successfully') {
-                const updatedCart = cart.filter(item => item.productId !== productId); // Ensure proper comparison of productId
+                const updatedCart = cart.filter(item => item.productId !== productId);
                 setCart(updatedCart);
             } else if (data.error === 'Item not found in cart') {
                 Swal.fire({
@@ -146,41 +124,40 @@ function Cart() {
                 text: 'Failed to remove item from cart.'
             });
         });
-    };    
-    
-    
+    };
+
     const handleClearCart = () => {
-        fetch(`${process.env.REACT_APP_API_URL}/carts/clear-cart`, {
-            method: "PUT",
+        fetch(`http://ec2-18-217-154-136.us-east-2.compute.amazonaws.com/b5/cart/clear-cart`, {
+            method: "DELETE",
             headers: {
                 "Content-Type": "application/json",
                 Authorization: `Bearer ${localStorage.getItem('token')}`
             }
         })
-            .then(res => res.json())
-            .then(data => {
-                if (data.message === 'Cart cleared successfully') {
-                    setCart([]);
-                } else {
-                    Swal.fire({
-                        icon: 'error',
-                        title: 'Error',
-                        text: 'Failed to clear cart.'
-                    });
-                }
-            })
-            .catch(err => {
+        .then(res => res.json())
+        .then(data => {
+            if (data.message === 'Cart cleared successfully') {
+                setCart([]);
+            } else {
                 Swal.fire({
                     icon: 'error',
                     title: 'Error',
                     text: 'Failed to clear cart.'
                 });
+            }
+        })
+        .catch(err => {
+            Swal.fire({
+                icon: 'error',
+                title: 'Error',
+                text: 'Failed to clear cart.'
             });
+        });
     };
 
     const handleCheckout = async () => {
         try {
-            const response = await fetch(`${process.env.REACT_APP_API_URL}/orders/checkout`, {
+            const response = await fetch(`http://ec2-18-217-154-136.us-east-2.compute.amazonaws.com/b5/order/checkout`, {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
@@ -190,8 +167,6 @@ function Cart() {
             const data = await response.json();
             if (response.ok) {
                 navigate("/order"); 
-                setOrderCreated(true); 
-                setCart([]);
                 Swal.fire({
                     icon: 'success',
                     title: 'Success',
@@ -199,6 +174,7 @@ function Cart() {
                     showConfirmButton: false,
                     timer: 1500
                 });
+                setCart([]);
             } else {
                 throw new Error(data.error || 'Failed to place order');
             }
@@ -212,38 +188,41 @@ function Cart() {
             });
         }
     };
-    
 
     return (
         <Container className="mt-5">
             <Row className="justify-content-center">
+                {isLoading? <p>Loading</p>:
                 <Col lg={8}>
                     <h2>Your Cart</h2>
-                    {cart.length === 0 ? (
+                    {cart.cartItems.length === 0? (
                         <p>Your cart is empty.</p>
                     ) : (
                         <div>
-                        {cart.map(item => (
-    <Card key={item.productId || 'defaultKey'} className="mb-3"> {/* Use a default key if productId is null */}
-        <Card.Body>
-            <Card.Title>{item.name}</Card.Title> {/* Display product name */}
-            <Card.Text>Quantity: {item.quantity}</Card.Text>
-            <Card.Text>Subtotal: {item.subtotal}</Card.Text>
-            <Button variant="outline-primary" style={{ marginRight: '5px', backgroundColor: '#934647', borderColor: '#934647' }} onClick={() => handleUpdateQuantity(item.productId, item.quantity + 1)}>
-                <AiOutlinePlus style={{ color: 'white' }} />
-            </Button>
-            <Button variant="outline-danger" style={{ marginRight: '5px', backgroundColor: '#934647', borderColor: '#934647' }} onClick={() => handleUpdateQuantity(item.productId, item.quantity - 1)}>
-                <AiOutlineMinus style={{ color: 'white' }} />
-            </Button>
-            <Button variant="outline-danger" style={{ backgroundColor: '#934647', borderColor: '#934647' }} onClick={() => handleRemoveItem(item.productId)}>
-                <AiOutlineDelete style={{ color: 'white' }} />
-            </Button>
-        </Card.Body>
-    </Card>
-))}
-                                            <p>Total Price: {totalPrice}</p>
+                            {cart.cartItems.map(item => (
+                                <Card key={item._id} className="mb-3">
+                                    <Card.Body>
+                                        <Card.Title>{item.productId.name}</Card.Title>
+                                        <Card.Text>Quantity: {item.quantity}</Card.Text>
+                                        <Card.Text>Subtotal: {item.subtotal}</Card.Text>
+                                        <Button variant="outline-primary" style={{ marginRight: '5px', backgroundColor: '#934647', borderColor: '#934647' }} onClick={() => handleUpdateQuantity(item.productId._id, item.quantity + 1)}>
+                                            +
+                                        </Button>
+                                        <Button variant="outline-danger" style={{ marginRight: '5px', backgroundColor: '#934647', borderColor: '#934647' }} onClick={() => handleUpdateQuantity(item.productId._id, item.quantity - 1)}>
+                                            -
+                                        </Button>
+                                        <Button variant="outline-danger" style={{ backgroundColor: '#934647', borderColor: '#934647' }} onClick={() => handleRemoveItem(item._id)}>
+                                            Remove
+                                        </Button>
+                                    </Card.Body>
+                                </Card>
+                            ))}
+                            <p>Total Price: {totalPrice}</p>
                             <Button variant="success" onClick={handleCheckout}>
                                 Checkout
+                            </Button>
+                            <Button variant="danger" onClick={handleClearCart} style={{ marginLeft: '5px' }}>
+                                Clear Cart
                             </Button>
                             <Link to="/products">
                                 <Button variant="primary" style={{ marginLeft: '5px', backgroundColor: '#934647', borderColor: '#934647' }}>Add More Items</Button>
@@ -251,9 +230,11 @@ function Cart() {
                         </div>
                     )}
                 </Col>
+            }
             </Row>
         </Container>
     );
 }
 
 export default Cart;
+
